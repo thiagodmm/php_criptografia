97fe6dee68ed90306bfe1fdf5a9ee158087ae975
thiagodmm <31706203+thiagodmm@users.noreply.github.com>
2021-12-03 11:21:40 -0300

    Início da Session - Atualização do index.php

--- index.php
+++ index.php
@@ -8,24 +8,7 @@
 </head>
 <body>
     <?php
-        $pdo = require_once './pdo/connection.php';
-
-        // $sql = 'insert into pessoa (nome,cpf) values (?,?)';
-        // $statement = $pdo->prepare($sql);
-        // $statement->execute(['Jack','123456']);
-
-        foreach ($pdo->query('select * from pessoa') as $row){
-            print $row['nome'] . "\t";
-            print $row['telefone'] . "\t";
-            print $row['email'] . "\t";
-            print $row['endereco'] . "\t";
-            print $row['cpf'] . "\t";
-            print $row['sexo'] . "\t";
-            print $row['cnpj'] . "\t";
-            print $row['nomeFantasia'] . "\t";
-            echo "<br>";
-
-        }
+    
     ?>
 
 </body>
